267e97cef9e033ba65c727347478c630927c7704
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-05-20 22:14:53 +0100

    commit tree 5adf4de58bb470e84ce2427a35fbc6f49fdc6c0f

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector("#modal")
+  const hearts = document.getElementsByClassName("like-glyph")
+  // const hearts = document.querySelectorAll("span.like-glyph")
+  for (const heart of hearts) {
+  heart.addEventListener("click", (e) => {
+    // make a server call
+    mimicServerCall() // return a promise 
+    // promises have the .then()
+    .then(() => {
+      if (heart.innerHTML == EMPTY_HEART){
+        heart.innerHTML = FULL_HEART 
+        heart.className = "activated-heart"
+      } else {
+        heart.innerHTML = EMPTY_HEART
+        heart.className = "like-glyph"
+      } 
+    })
+    .catch(error => {
+      modal.hidden = false
+      const modalMessage = document.querySelector("#modal-message")
+      modalMessage.innerText = error
+      setTimeout(() => {
+        modal.hidden = true
+       }, 5000)
+    })
+  })
+  }
+  console.log(hearts)
+})
 
 
 
